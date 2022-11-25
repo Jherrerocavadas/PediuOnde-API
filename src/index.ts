f58425a351c78516c981d2a63c761a9bbb7298dc
Jherrerocavadas <jherrerocavadas@gmail.com>
2022-11-25 21:09:27 -0200

    Inclusão das rotas na API

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 
+import { router } from "./router";
+
 mongoose.connect("mongodb://localhost:27017")
 .then(() => {
 
   const api = express();
   const port = 3001;
+  api.use(express.json());
+  api.use(router)
 
   api.listen(port, () =>{
     console.log(`Servidor da API PediuOnde? Online em http://localhost:${port}`)
   }
 );})
-.catch(() => console.log("deu doidera"));
+.catch(() => console.log("Erro da API: Não foi possível se conectar ao MongoDB"));
